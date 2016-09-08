c4a684324ba5cda1e2c00a0954833daddc0fd82e
Eric Parker <theriex@gmail.com>
2016-09-08 17:19:45 -0400

    moved menu to left, factored content into single site

--- klsgae/docroot/js/app.js
+++ klsgae/docroot/js/app.js
@@ -1,6 +1,6 @@
-/*global alert: false, console: false, confirm: false, setTimeout: false, window: false, document: false, history: false, navigator: false, jtminjsDecorateWithUtilities: false */
+/*global jtminjsDecorateWithUtilities, window */
 
-/*jslint regexp: true, unparam: true, white: true, maxerr: 50, indent: 4 */
+/*jslint browser, multivar, white, fudge, for */
 
 var app = {},
     jt = {};
@@ -13,7 +13,8 @@ var app = {},
     ////////////////////////////////////////
 
     var addr = 
-        ['k', 'a', 'r', 'e', 'n', '.', 's', 'u', 'y', 'e', 'm', 'o', 't', 'o'],
+        ["k", "a", "r", "e", "n", ".", 
+         "s", "u", "y", "e", "m", "o", "t", "o"],
 
 
     ////////////////////////////////////////
@@ -31,36 +32,76 @@ var app = {},
         var idx;
         if(!html || !html.trim()) {
             html = url + " contains no text"; }
-        idx = html.indexOf("<body>");
+        idx = html.indexOf("<body");
         if(idx > 0) {
-            html = html.slice(idx + "<body>".length,
-                              html.indexOf("</body")); }
-        jt.out('contentdiv', html);
+            html = html.slice(idx);
+            idx = html.indexOf(">");
+            html = html.slice(idx + 1, html.indexOf("</body")); }
+        jt.out("contentdiv", html);
     },
 
 
     attachDocLinkClick = function (node, link) {
-        var pes = link.split("/");
+        var pes, src, fname, html;
+        pes = link.split("/");
         if(pes.length >= 2) {
-            if(pes[pes.length - 2] === "KLSweb") {
-                node.href = "index.html?site=" + pes[pes.length - 2] +
-                    "&file=" + pes[pes.length - 1]; }
-            else if(pes[pes.length - 2] === "AARTweb") {
-                node.href = "AARTindex.html?site=" + pes[pes.length - 2] +
-                    "&file=" + pes[pes.length - 1]; } }
+            src = pes[pes.length - 2];
+            fname = pes[pes.length - 1];
+            if(src === "html") {
+                node.href = "index.html?sd=html&fn=" + fname; }
+            else if(src == "pdf") {
+                html = ["a", {href: "pdf/" + fname,
+                              onclick: jt.fs("window.open('pdf/" + 
+                                             fname + "')")},
+                        node.innerHTML];
+                node.outerHTML = jt.tac2html(html); } }
     },
 
 
     localDocLinks = function () {
         var i, nodes, node, href;
-        nodes = document.getElementsByTagName('a');
+        nodes = document.getElementsByTagName("a");
         for(i = 0; nodes && i < nodes.length; i += 1) {
             node = nodes[i];
             href = node.href;
-            //href may have been resolved from relative to absolute...
+            //browser may resolve href relative path to absolute
             if(href && href.indexOf("index.html") < 0) {
                 attachDocLinkClick(node, href); } }
     };
+
+
+    //Sometimes there's a significant lag loading the fonts, and if
+    //that happens the index page should not be waiting to display
+    //because that just makes the whole site look slow. To avoid that
+    //lag, load the fonts last.  If the fonts load fast, the update
+    //will be imperceptible.  If they load slow then at least the
+    //content will be available to read in the meantime.
+    function addFontSupport () {
+        var fontlink = document.createElement("link");
+        fontlink.href = "http://fonts.googleapis.com/css?family=Asap";
+        fontlink.rel = "stylesheet";
+        fontlink.type = "text/css";
+        document.getElementsByTagName("head")[0].appendChild(fontlink);
+    }
+
+
+    function initMenu () {
+        var html, contentdiv;
+        html = ["a", {href: "#menu", title: "Show Menu",
+                      onclick: jt.fs("app.toggleMenu()")},
+                [["img", {src: "img/bmenuico.png"}],
+                 "menu"]];
+        jt.out("menuaccessdiv", jt.tac2html(html));
+        contentdiv = jt.byId("menucontentdiv");
+        html = ["div", {id: "closemenuxdiv", 
+                        style: "float:right;margin:5px;"},
+                ["a", {href: "#closemenu", title: "Hide Menu",
+                       onclick: jt.fs("app.toggleMenu()")},
+                 "[x]"]];
+        html = jt.tac2html(html) + contentdiv.innerHTML;
+        contentdiv.innerHTML = html;
+        contentdiv.style.display = "none";
+    }
 
 
     ////////////////////////////////////////
@@ -69,34 +110,55 @@ var app = {},
 
     app.displayDoc = function (url) {
         var html = "Retrieving " + url + " ...";
-        jt.out('contentdiv', html);
+        jt.out("contentdiv", html);
         if(url.indexOf(":") < 0) {
             url = relativeToAbsolute(url); }
-        jt.request('GET', url, null,
+        jt.request("GET", url, null,
                    function (resp) {
                        displayDocContent(url, resp); },
                    function (code, errtxt) {
-                       displayDocContent(url, errtxt); },
+                       displayDocContent(url, String(code) + ": " + errtxt); },
                    jt.semaphore("app.displayDoc"));
     };
 
 
     app.contact = function () {
-        var html = jt.byId('clink').innerHTML;
-        html = ["a", {href: "mailto:" + addr.join("") + "@umb.edu"},
-                html];
-        jt.out('clink', jt.tac2html(html));
+        var span, html;
+        span = jt.byId("clink");
+        if(span) {
+            html = span.innerHTML;
+            html = ["a", {href: "mailto:" + addr.join("") + "@umb.edu"},
+                    html];
+            jt.out("clink", jt.tac2html(html)); }
     };
 
 
     app.init = function () {
-        var pobj;
+        var pobj, content;
         jtminjsDecorateWithUtilities(jt);
         pobj = jt.paramsToObj(window.location.search, {}, "String");
-        if(pobj.site && pobj.file) {
-            app.displayDoc(pobj.site + "/" + pobj.file); }
+        content = "html/intro.html";
+        if(pobj.sd && pobj.fn) {
+            content = pobj.sd + "/" + pobj.fn; }
+        app.displayDoc(content);
+        initMenu();  //rewrites menu doc html
         localDocLinks();
         setTimeout(app.contact, 4000);
+        addFontSupport();
+    };
+
+
+    app.toggleMenu = function () {
+        var mc, macc;
+        mc = jt.byId("menucontentdiv");
+        if(mc) {
+            macc = jt.byId("menuaccessdiv");
+            if(mc.style.display === "block") {
+                macc.style.display = "block";
+                mc.style.display = "none"; }
+            else {
+                macc.style.display = "none";
+                mc.style.display = "block"; } }
     };
 
 
